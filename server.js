0ec1102f040ff0df4fe98206570fab19b1540f17
m-aung <myothuwinaung1991@gmail.com>
2021-05-11 00:59:22 -0400

    updated gitignore packa.json server.js

--- server.js
+++ server.js
@@ -1,36 +1,35 @@
-const express = require ('express');
+const express = require('express');
 // body parser is to address the data in object form
-const bodyParser = require ('body-parser');
-const MongoClient = require('mongodb').MongoClient
+const MongoClient = require('mongodb').MongoClient;
 const app = express();
-
-var db;
-
+const dotenv = require('dotenv');
+let db;
+dotenv.config();
 // username = test-user
 // password = 22102008
 
 // connecting to Database
-MongoClient.connect('mongodb+srv://test-user:22102008@cluster0-m1plq.mongodb.net/test?retryWrites=true&w=majority', function (err, database) {
-    // ... start the server
-    if (err) return console.log(err);
-    db = database;
-    app.listen(3000, function() {
-        console.log('listening to port 3000');
-    });
-})
-
-app.use(bodyParser.urlencoded({extended: true}));
-
-// app.get(path, callback);
-app.get('/',function (req, res) {
-    // res.send('This is a GET');
-    res.sendFile(__dirname + '/index.html');
+MongoClient.connect(process.env.DB_URL, (err, database) => {
+  // ... start the server
+  if (err) return console.log(err);
+  db = database;
+  app.listen(3000, function () {
+    console.log('listening to port 3000');
+  });
 });
 
-app.post('/todo', function(req, res) {
-    db.collection('todo').save(req.body, function (err, result){
-        if(err)return cole.log(err);
-        console.log('saved to our db');
-        res.redirect('/');
-    })
+app.use(express.urlencoded({ extended: true }));
+
+// app.get(path, callback);
+app.get('/', function (req, res) {
+  // res.send('This is a GET');
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/todo', function (req, res) {
+  db.collection('todo').save(req.body, function (err, result) {
+    if (err) return cole.log(err);
+    console.log('saved to our db');
+    res.redirect('/');
+  });
 });
